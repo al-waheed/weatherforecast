db4e566ef1ce15152106e5f88dd7bac3c0a1969e
morenikeji ajisegiri <ajisegirimorenikeji@gmail.com>
2021-06-26 09:42:48 +0100

    weather project

---
+++ src/WeatherDataContext.js
@@ -0,0 +1,38 @@
+import React, { Component, createContext } from 'react'
+
+export const WeatherDataContext = createContext();
+
+export class WeatherProvider extends Component {
+	state = {
+		apiKey: "c173054579f97ce5b2826f318e5cf6f8",
+		apiUrl: "https://api.openweathermap.org/data/2.5/",
+		query: '',
+		weather: {}
+	};
+
+	handleSearch = (event) => {
+		const { apiUrl, apiKey, query } = this.state
+		if (event.key === "Enter") {
+			fetch(`${apiUrl}weather?q=${query}&units=metric&APPID=${apiKey}`)
+				.then(res => res.json())
+				.then(res => {
+					this.setState({ weather: res })
+					this.setState({ query: '' })
+				});
+		}
+	}
+
+	handleChange = (e) => {
+		this.setState({ query: e.target.value });
+	};
+
+	render() {
+		const { query, weather } = this.state
+		const { handleChange, handleSearch } = this;
+		return (
+			<WeatherDataContext.Provider value={{ query, weather, handleChange, handleSearch }}>
+				{this.props.children}
+			</WeatherDataContext.Provider>
+		)
+	}
+}
